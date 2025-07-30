1124fef6ccb73771557839dce7adbcb0e40c0aa3
Dennis den Ouden-van der Horst <d.denouden-vanderhorst@tudelft.nl>
2025-07-30 15:55:09 +0200

    Include translation files in package data
    Added translation-related files and directories to the package_data section in setup.py to ensure they are included in the distribution. This supports proper packaging and deployment of translation resources.

--- setup.py
+++ setup.py
@@ -24,7 +24,15 @@ setup(
     license="MIT License",
     packages=find_packages(),
     package_data={
-        "sphinx_togglebutton": ["_static/togglebutton.css", "_static/togglebutton.js", "_static/togglebutton-chevron.svg"]
+        "sphinx_togglebutton": [
+            "_static/togglebutton.css", 
+            "_static/togglebutton.js", 
+            "_static/togglebutton-chevron.svg",
+            "translations/README.md",
+            "translations/_convert.py",
+            "translations/jsons/*.json",
+            "translations/locales/**/*"
+        ]
     },
     install_requires=["setuptools", "wheel", "sphinx", "docutils"],
     extras_require={"sphinx": ["matplotlib", "numpy", "myst_nb", "sphinx_book_theme", "sphinx_design", "sphinx_examples"]},
